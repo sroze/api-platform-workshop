267c04c52afedc2a603a05220241e4f8fc57a84c
Samuel ROZE <samuel.roze@gmail.com>
2016-09-14 17:50:23 +0100

    Create a custom action

--- src/AppBundle/Entity/Book.php
+++ src/AppBundle/Entity/Book.php
@@ -8,7 +8,10 @@ use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
- * @ApiResource
+ * @ApiResource(itemOperations={
+ *     "get"={"method"="GET"},
+ *     "buy"={"route_name"="book_buy"}
+ * })
  *
  * @ORM\Entity
  */
@@ -33,7 +36,7 @@ class Book
     /**
      * @ORM\OneToMany(targetEntity="Review", mappedBy="book")
      *
-     * @var Review[]
+     * @var Collection
      */
     private $reviews;
 
